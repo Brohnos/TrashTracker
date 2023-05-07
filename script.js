238599757384ca09ab53300c389e73feac57f624
Brohnos <131167983+Brohnos@users.noreply.github.com>
2023-05-07 22:25:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ const firebaseConfig = {
   measurementId: "G-SZZW59VS4Q"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-const db = firebase.database();
+const database = firebase.database();
 
 document.addEventListener('DOMContentLoaded', function () {
   var map = L.map('map');
@@ -23,12 +22,13 @@ document.addEventListener('DOMContentLoaded', function () {
   }).addTo(map);
 
   setMapViewToFaithlegg(map);
-
-  // Attempt to set the map view to the user's location and add a pin at their location
   setMapViewToUserLocationAndAddPin(map);
+  enableManualPinDrop(map);
 
-  // Load pins from the database
-  loadPinsFromDatabase(map);
+  database.ref('pins/').on('child_added', (snapshot) => {
+    const pinData = snapshot.val();
+    addPin(map, [pinData.latitude, pinData.longitude], pinData.comment, pinData.id);
+  });
 });
 
 function setMapViewToFaithlegg(map) {
@@ -42,68 +42,50 @@ function setMapViewToUserLocationAndAddPin(map) {
       (position) => {
         const userLocation = [position.coords.latitude, position.coords.longitude];
         map.setView(userLocation, 14);
-        enableManualPinDrop(map);
       },
       (error) => {
         console.log('Error getting user location:', error);
-        enableManualPinDrop(map);
       }
     );
   } else {
     console.log('Geolocation is not supported by this browser.');
-    enableManualPinDrop(map);
   }
 }
 
-function addPin(map, location, comment = '') {
+function addPin(map, location, comment, pinId) {
   const marker = L.marker(location).addTo(map);
-  marker.on('click', function () {
-    if (confirm('Do you want to delete this pin?')) {
-      map.removeLayer(marker);
-      deletePinFromDatabase(marker);
-    }
-  });
 
   if (comment) {
     marker.bindPopup(comment);
   }
 
-  return marker;
-}
+  if (pinId) {
+    marker.pinId = pinId;
+  }
 
-function enableManualPinDrop(map) {
-  map.on('click', function (e) {
-    const comment = prompt('Please enter a comment for this pin:');
-    if (comment) {
-      const marker = addPin(map, e.latlng, comment);
-      savePinToDatabase(marker, comment);
+  marker.on('click', function () {
+    if (confirm('Do you want to delete this pin?')) {
+      map.removeLayer(marker);
+      if (marker.pinId) {
+        database.ref('pins/' + marker.pinId).remove();
+      }
     }
   });
 }
 
-function savePinToDatabase(marker, comment) {
-  const { lat, lng } = marker.getLatLng();
-  const newPinRef = db.ref('pins').push();
-  newPinRef.set({ lat, lng, comment });
-  marker.pinKey = newPinRef.key; // Store the key on the marker for future reference
-}
-
-function deletePinFromDatabase(marker) {
-  if (marker.pinKey) {
-    db.ref('pins/' + marker.pinKey).remove();
-  }
-}
-
-function loadPinsFromDatabase(map) {
-  db.ref('pins').on('value', function (snapshot) {
-    const data = snapshot.val();
-    if (data) {
-      for (const pinKey in data) {
-        const pinData = data[pinKey];
-        const latLng = L.latLng(pinData.lat, pinData.lng);
-        const marker = addPin(map, latLng, pinData.comment);
-        marker.pinKey = pinKey; // Store the key on the marker for future reference
-      }
+function enableManualPinDrop(map) {
+  map.on('click', function (e) {
+    const location = e.latlng;
+    const comment = prompt('Please enter a comment for this pin:');
+    if (comment) {
+      const pinData = {
+        latitude: location.lat,
+        longitude: location.lng,
+        comment: comment
+      };
+      const newPinRef = database.ref('pins/').push();
+      pinData.id = newPinRef.key;
+      newPinRef.set(pinData);
     }
   });
 }
